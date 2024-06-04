e2eeed10f12c2102a513ee590bc3507f1c99f244
Goudmu <goudmu@gmail.com>
2024-06-04 11:22:03 +0700

    fix options back up

--- backup/optionsbackup.ts
+++ backup/optionsbackup.ts
@@ -1,5 +1,6 @@
 import { User2 } from "@/lib/mongodb/models";
-import { AuthOptions } from "next-auth";
+import { AuthOptions, User } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import CredentialProvider from "next-auth/providers/credentials";
 
 export const options: AuthOptions = {
@@ -60,26 +61,28 @@ export const options: AuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async jwt({
-      token,
-      user,
-      trigger,
-      session,
-    }: {
-      token: any;
-      user?: any;
-      trigger: any;
-      session: any;
+    async jwt(params: {
+      token: JWT;
+      user: User | null;
+      trigger?: "signIn" | "signUp" | "update" | undefined;
+      session?: any;
     }) {
+      const { token, user, trigger, session } = params;
       if (trigger === "update") {
         return { ...token, ...session.user };
       }
       if (user) {
+        if ("username" in user) {
+          token.username = user.username;
+        }
+        if ("role" in user) {
+          token.role = user.role;
+        }
+        if ("password" in user) {
+          token.password = user.password;
+        }
         token.id = user.id;
-        token.username = user.username;
         token.email = user.email;
-        token.role = user.role;
-        token.password = user.password;
       }
       return token;
     },
