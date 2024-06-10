dbf08b613b454c156a3ce401c3329218d5890ff3
Goudmu <goudmu@gmail.com>
2024-06-11 06:46:45 +0700

    sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://kasmasjidfix.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://kasmasjidfix.vercel.app/public/laporan",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+  ];
+}
